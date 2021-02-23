2ad60457df9c7851bdd152b3d968349e15d96c4a
crazyilian <ilian.and.04@gmail.com>
2021-02-23 23:46:47 +0300

    feat: send message on bot start. start work on settings

--- bot.py
+++ bot.py
@@ -1,5 +1,7 @@
 from telethon import events
 from telethon.tl.types import MessageMediaPhoto
+from telethon.tl.custom import Button
+from telethon.errors.rpcerrorlist import MessageNotModifiedError
 from Demotivator import make_demotivator
 import logging
 from Photo import *
@@ -7,6 +9,8 @@ from Common import *
 from Fonts import get_font_path
 from time import time
 import re
+import traceback
+import asyncio
 
 
 BOTNAME = 'CreateDemotivatorBot'
@@ -15,14 +19,13 @@ logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s'
                     level=logging.WARNING)
 
 
-@bot.on(events.NewMessage(pattern=fr'(?i)^/make({BOTNAME}|)(\s.*|$)', incoming=True))
-async def reply(event):
-    id = event.chat_id
+@bot.on(events.NewMessage(pattern=fr'(?i)^/make({BOTNAME}|)(\s|$)', incoming=True))
+async def make(event):
     message = event.message
     reply = await event.get_reply_message()
     if reply is None or not isinstance(reply.media, MessageMediaPhoto) or reply.media.photo is None:
-        await bot.send_message(id, 'To create demotivator, **reply** to a message with a **photo** and add command'
-                                   ' `/make` with text.\nCommand example: `/make What? :: It\'s magic`')
+        await event.respond('To create demotivator, **reply** to a message with a **photo** and add command'
+                            ' `/make` with text.\nCommand example: `/make What? :: It\'s magic`')
         return
     photo = await save_photo(reply.media.photo)
     text = message.raw_text
@@ -38,5 +41,80 @@ async def reply(event):
     demotivator = make_demotivator(photo, title=title, caption=caption,
                                    font=get_font_path("ThamesC"),
                                    font_caption=get_font_path("Verdana"))
-    my_message = await bot.send_message(id, file=get_path(demotivator))
+    my_message = await event.respond(file=get_path(demotivator))
     link_photo_id(my_message.media.photo.id, demotivator)
+
+
+def text_wordwrap(text_type, chosen):
+    def button(text, data):
+        return Button.inline(
+            '✅ ' * (chosen.lower() == data.lower()) + text,
+            text_type + '/wordwrap/' + data
+        )
+
+    text = f'**Word wrap settings for {text_type}**\n' \
+           '• **Custom** - only your new lines are used\n' \
+           '• **Auto wrap** - only auto new lines are used\n' \
+           '• **Mixed** - your new lines are used but auto wrap can add more new lines\n\n' \
+           '__Auto wrap will add new lines if text size reaches minimum size limit__'
+    buttons = [
+        [button('Custom', 'custom'), button('Auto wrap', 'auto'), button('Mixed', 'mixed')],
+        [button('Back', 'back')]
+    ]
+    return text, buttons
+
+
+user_data = {'404377069': {
+    'title': {'font': 'thamesc', 'bold': False, 'italic': False, 'size': {'type': 'auto', 'auto': [10, 80], 'fixed': 80},
+              'wordwrap': 'mixed'},
+    'caption': {'font': 'verdana', 'bold': False, 'italic': False, 'size': {'type': 'auto', 'auto': [10, 29], 'fixed': 29},
+                'wordwrap': 'mixed'},
+    'image': {}
+}}
+
+
+def message_by_path(id, path):
+    parts = path.split('/')
+    if path in ('title', 'caption'):
+        pass
+
+
+# @bot.on(events.NewMessage(pattern=fr'(?i)^/text({BOTNAME}|)(\s|$)', incoming=True))
+# async def settings(event):
+#     text, buttons = text_wordwrap('title', 'mixed')
+#     await event.respond(text, buttons=buttons)
+
+
+processing_callbacks = set()
+
+
+@bot.on(events.CallbackQuery())
+async def callbackhandler(event):
+    id = event.chat_id
+    if id in processing_callbacks:
+        await event.answer()
+        print('oh')
+        return
+    processing_callbacks.add(id)
+
+    # # # # # # # # # # # # # # # #
+
+    path = event.data.decode().split('/')
+    val = path[-1]
+    if val == 'back':
+        text = 'lol'
+        buttons = None
+    elif path[1] == 'wordwrap':
+        text, buttons = text_wordwrap(path[0], val)
+
+    # # # # # # # # # # # # # # # #
+
+    try:
+        await event.edit(text, buttons=buttons)
+    except MessageNotModifiedError:
+        print(f'Message Not Modified: too more queries from {id}')
+        await asyncio.sleep(2)
+    except Exception as e:
+        traceback.print_exc()
+        await asyncio.sleep(2)
+    processing_callbacks.remove(id)
--- run.py
+++ run.py
@@ -3,9 +3,19 @@ import bot as bot_handlers
 from Common import *
 
 
+async def f():
+    from datetime import datetime
+    a = datetime.now()
+    s = 'Current date\n' + a.strftime('%d %B %Y %H:%M:%S')
+    await bot.send_message(404377069, s)
+
+
+
 try:
     fix_tmp()
     clear_tmp()
+    # bot.send_message()
+    bot.loop.run_until_complete(f())
     bot.run_until_disconnected()
 except Exception as e:
     clear_tmp()
